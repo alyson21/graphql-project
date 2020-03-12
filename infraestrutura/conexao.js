a09eede7b215c02805825e8230518d853860ef83
alyson <alysonroliveira@hotmail.com>
2020-03-12 19:50:52 -0300

    adaptação para mysql

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -1,20 +1,12 @@
-const mssql = require('mssql')
+const mysql = require('mysql')
 
-const conexao = new mssql.ConnectionPool({
-  user: 'teknisa',
-  password: 'teknisa',
-  server: 'TEK-PDT007\\SQLEXPRESS',
-  database: 'agendapetshop',
+
+const conexao = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '',
+  database: 'agendapetshop'
 })
-
-// conexao = mssql.connect('mssql://teknisa:teknisa@TEK-PDT007\SQLEXPRESS/master')
-
-// const conexao = mssql.connect({
-//   host: 'localhost',
-//   port: 3306,
-//   user: 'teknisa',
-//   password: 'teknisa',
-//   database: 'agenda-petshop'
-// })
 
 module.exports = conexao
